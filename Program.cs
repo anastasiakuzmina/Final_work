d19162052058445d8f9f6962cefe18866e64982c
Anastasia <nastenka.antropova.96@mail.ru>
2022-11-07 22:28:39 +0300

    итоговая работа V1.2

--- Program.cs
+++ Program.cs
@@ -13,38 +13,3 @@ string [] GetShortStr (string [] str)
  Array.Resize(ref resultstr, k); // меняем размерность массива на новую 
 return resultstr;
 }
-
-void Printstr(string[] str)
-{
-    if (str.Length == 0) // Проверяем пустой ли массив
-    {
-        Console.WriteLine("Массив пуст");
-    }
-    else
-    {
-        Console.Write("[");
-        for (int i = 0; i < str.Length - 1; i++)
-        {
-            Console.Write($"\"{str[i]}\", ");
-        }
-        Console.Write($"\"{str[str.Length - 1]}\"");
-        Console.WriteLine("]");
-    }
-}
-
-string[] str1 = { "Hello", "2", "world", ":-)" };
-string[] str2 = { "1234", "1567", "-2", "computer science" };
-string[] str3 = { "Russia", "Denmark", "Kazan" };
-Console.WriteLine("Исходный массив:");
-Printstr(str1);
-Printstr(str2);
-Printstr(str3);
-string[] restr1 = GetShortStr(str1);
-string[] restr2 = GetShortStr(str2);
-string[] restr3 = GetShortStr(str3);
-Console.WriteLine($"Массив результирующий (строка меньше или равна 3 символам):");
-Printstr(restr1 );
-Printstr(restr2 );
-Printstr(restr3 );
-
-Console.WriteLine();
